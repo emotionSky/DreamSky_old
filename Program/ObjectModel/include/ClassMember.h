e8bd0f276aec2b3774fccdfb219a4d95dc004013
emotionsky <bitdreamsky@126.com>
2022-09-15 22:32:31 +0800

    增加一些使用技巧

--- Program/ObjectModel/include/ClassMember.h
+++ Program/ObjectModel/include/ClassMember.h
@@ -1,6 +1,23 @@
 ﻿#ifndef __CLASS_MEMBER_H__
 #define __CLASS_MEMBER_H__
 
+/**
+ * 查看类的对象数据布局：
+ * 1）vs查看方式：
+ *    打开vs的开发人员命令行工具，进入到cpp文件所在目录，然后执行命令  cl /dl reportSingleClassLayout{Name} {file}
+ *    示例：  cl /dl reportSingleClassLayoutClassMember ClassMember.cpp
+ * 2）gcc查看方式：
+ *    g++ -fdump-class-hierarchy -fsyntax-only {file}
+ * 
+ * 查看obj文件的内容：
+ * 1）vs查看方式：
+ *    打开vs的开发人员命令行工具，进入obj文件所在目录，然后执行命令  dumpbin /all {obj_file} > {name}.txt
+ *    示例：  dumpbin /all ClassMember.obj > ClassMember.txt
+ * 2）gcc查看方式：
+ *    g++ -E hello.cpp -o hello.i  //类似上面的txt
+ *    g++ -S hello.i -o hello.s    //汇编
+ */
+
 class ClassMember
 {
 public:
